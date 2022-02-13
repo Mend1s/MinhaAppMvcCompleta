578cbcad6d8a98a2593b255a69b3432eaea9f0ab
Mend1s <andremendesskt99@gmail.com>
2022-02-12 22:05:24 -0300

    Add project files.

---
+++ src/DevIO.Data/Mappings/EnderecoMapping.cs
@@ -0,0 +1,44 @@
+﻿using DevIO.Business.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DevIO.Data.Mappings
+{
+    public class EnderecoMapping : IEntityTypeConfiguration<Endereco>
+    {
+        public void Configure(EntityTypeBuilder<Endereco> builder)
+        {
+            builder.HasKey(keyExpression: p => p.Id);
+
+            builder.Property(c => c.Logradouro)
+                .IsRequired()
+                .HasColumnName("varchar(200)");
+
+            builder.Property(c => c.Numero)
+                .IsRequired()
+                .HasColumnName("varchar(50)");
+
+            builder.Property(c => c.Cep)
+                .IsRequired()
+                .HasColumnName("varchar(8)");
+
+            builder.Property(c => c.Complemento)
+                .IsRequired()
+                .HasColumnName("varchar(250)");
+
+            builder.Property(c => c.Bairro)
+                .IsRequired()
+                .HasColumnName("varchar(100)");
+
+            builder.Property(c => c.Cidade)
+                .IsRequired()
+                .HasColumnName("varchar(100)");
+
+            builder.Property(c => c.Estado)
+                .IsRequired()
+                .HasColumnName("varchar(50)");
+
+            builder.ToTable("Enderecos");
+        }
+    }
+}
---
+++ src/DevIO.Data/Mappings/ProdutoMapping.cs
@@ -0,0 +1,27 @@
+﻿using DevIO.Business.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DevIO.Data.Mappings
+{
+    public class ProdutoMapping : IEntityTypeConfiguration<Produto>
+    {
+        public void Configure(EntityTypeBuilder<Produto> builder)
+        {
+            builder.HasKey(keyExpression: p => p.Id);
+
+            builder.Property(p => p.Nome)
+                .IsRequired()
+                .HasColumnName("varchar(200)");
+
+            builder.Property(p => p.Descricao)
+                .IsRequired()
+                .HasColumnName("varchar(1000)");
+
+            builder.Property(p => p.Imagem)
+                   .IsRequired()
+                   .HasColumnName("varchar(100)");
+            builder.ToTable("Produtos");
+        }
+    }
+}
